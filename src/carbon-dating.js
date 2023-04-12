56be06a3c468b0338749ff50e565c6c7c553c8aa
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2023-04-12 21:48:25 +0400

    feat: add solution 4 tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,20 +6,29 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
   // remove line with error and write your code here
+  const logOfTwo = 0.693;
+  if (typeof(sampleActivity) === 'string' && Number(sampleActivity)) {
+    if (Number(sampleActivity) >= 1 && Number(sampleActivity) <= MODERN_ACTIVITY) {
+      const time = Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (logOfTwo / HALF_LIFE_PERIOD));
+      return time;
+    }
+    return false;
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -2,18 +2,35 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
   // remove line with error and write your code here
+  if(!date) return 'Unable to determine the time of year!';
+  if (!(date instanceof Date) || Object.getOwnPropertyNames(date)[0]) throw new Error('Invalid date!');
+  let month = date.getMonth();
+
+  switch (month) {
+    case 11: return 'winter';
+    case 0: return 'winter';
+    case 1: return 'winter';
+    case 2: return 'spring';
+    case 3: return 'spring';
+    case 4: return 'spring';
+    case 5: return 'summer';
+    case 6: return 'summer';
+    case 7: return 'summer';
+    case 8: return 'autumn';
+    case 9: return 'autumn';
+    case 10: return 'autumn';
+  }
 }
 
 module.exports = {
